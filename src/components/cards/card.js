8839ee954145606add003c3e280e1e20f41fca62
mirabbos2002 <69849085+mirabbos2002@users.noreply.github.com>
2021-08-05 09:48:16 +0500

    cards showed up

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -4,27 +4,29 @@ export default function Card() {
   const products = useSelector((state) => state.allProducts.products);
   let everyProduct = products.map((product) => {
     const { id, title, image, price, category } = product;
-    <div className="card" key={id}>
-      <div className="img-card">
-        <img width="100%" src={image} alt={title} />
-      </div>
-      <div className="flex content-card space-b">
-        <div className="card-box-left">
-          {title}
-          <br />
-          Size: 41
-          <br />
-          {category}
+    return (
+      <div className="card" key={id}>
+        <div className="img-card">
+          <img width="100%" src={image} alt={title} />
         </div>
-        <div className="card-box-right">
-          <i className="far fa-heart"></i>
-          <i className="fas fa-heart"></i>
-          <br />
-          <br />
-          Cost: {price}
+        <div className="flex content-card space-b">
+          <div className="card-box-left">
+            {title}
+            <br />
+            Size: 41
+            <br />
+            {category}
+          </div>
+          <div className="card-box-right">
+            <i className="far fa-heart"></i>
+            <i className="fas fa-heart"></i>
+            <br />
+            <br />
+            Cost: {price}
+          </div>
         </div>
       </div>
-    </div>;
+    );
   });
   console.log(products);
   return <>{everyProduct}</>;
